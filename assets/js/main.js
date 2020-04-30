7efd389cbff215b2858d870d92db56fcfd53b620
Seed Zeng <cdzengpeiyun@gmail.com>
2020-04-30 19:37:30 -0400

    Improve loading 1. get rid of preloader 2. reduce image sizes 3. improve JS loading time

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,4 @@
-(function ($) {
+$(document).ready(function ($) {
 
     // $(".rippler").rippler({
     //     effectClass: 'rippler-effect'
@@ -151,7 +151,30 @@
     });
 
 
+    /* Animations on scroll */
+    $('.js--wp-1').waypoint(function (direction) {
+        $('.js--wp-1').addClass('animated pulse');
+    }, {
+        offset: '50%'
+    });
 
+    $('.js--wp-2').waypoint(function (direction) {
+        $('.js--wp-2').addClass('animated tada');
+    }, {
+        offset: '50%'
+    });
+
+    $('.js--wp-3').waypoint(function (direction) {
+        $('.js--wp-3').addClass('animated swing');
+    }, {
+        offset: '50%'
+    });
+
+    $('.js--wp-4').waypoint(function (direction) {
+        $('.js--wp-4').addClass('animated pulse');
+    }, {
+        offset: '50%'
+    });
     //=====
 
 }(jQuery));
